c29a36ff1baba3e31dee1e5ed84f5c47b79a0540
fredribeiro97@gmail.com <fredribeiro97@gmail.com>
2019-01-09 14:37:55 -0200

    Refresh token

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -19,6 +19,10 @@ export class AuthService{
         return this.http.post(`${API_CONFIG.baseUrl}/login`, creds, {observe: 'response', responseType: 'text'});
     }
 
+    refreshToken(){
+        return this.http.post(`${API_CONFIG.baseUrl}/auth/refresh_token`, {}, {observe: 'response', responseType: 'text'});
+    }
+
     successfulLogin(authorizationValue: string){
         let tok = authorizationValue.substring(7);
         let user : LocalUser = {token: tok, email: this.jwtHelper.decodeToken(tok).sub};
