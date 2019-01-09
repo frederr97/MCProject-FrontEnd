fe0755a97851a6317885e60b96e85c63d8b4d039
fredribeiro97@gmail.com <fredribeiro97@gmail.com>
2019-01-09 00:04:13 -0200

    Interceptor implementation

--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -1,30 +1,28 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { CategoriaService } from '../../services/domain/categoria.service';
-
-/**
- * Generated class for the CategoriasPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { API_CONFIG } from '../../config/api.config';
+import { CategoriaDTO } from '../../models/categoria.dto';
 
 @IonicPage()
 @Component({
   selector: 'page-categorias',
   templateUrl: 'categorias.html',
 })
-export class CategoriasPage {
+
+export default class CategoriasPage {
+
+  bucketUrl: string = API_CONFIG.bucketBaseUrl;
+
+  items: CategoriaDTO[];
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public categoriaService: CategoriaService) {
   }
 
   ionViewDidLoad() {
     this.categoriaService.findAll().subscribe(response => {
-      console.log(response);
+      this.items = response;
     },
-    error => {
-      console.log(error);
-    });
+    error => {});
   }
 }
