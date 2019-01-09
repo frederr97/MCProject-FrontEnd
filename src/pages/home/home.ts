8cb0b3b7e5c7785fed9d65830d7d5970ebb2de8e
fredribeiro97@gmail.com <fredribeiro97@gmail.com>
2019-01-09 00:26:26 -0200

    Authorization implementation

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, IonicPage, MenuController } from 'ionic-angular';
+import { CredenciaisDTO } from '../../models/credenciais.dto';
+import { AuthService } from '../../services/auth.service';
 
 @IonicPage()
 @Component({
@@ -9,7 +11,12 @@ import { NavController, IonicPage, MenuController } from 'ionic-angular';
 
 export class HomePage {
 
-  constructor(public navCtrl: NavController, public menu: MenuController) {
+  creds : CredenciaisDTO = {
+    email: "",
+    senha: ""
+  };
+
+  constructor(public navCtrl: NavController, public menu: MenuController, public auth: AuthService) {
 
   }
 
@@ -22,6 +29,10 @@ export class HomePage {
   }
 
   login(){
-    this.navCtrl.setRoot('CategoriasPage');
+    this.auth.authenticate(this.creds).subscribe(response => {
+      console.log(response.headers.get('Authorization'));
+      this.navCtrl.setRoot('CategoriasPage');
+    },
+    error => {});
   }
 }
