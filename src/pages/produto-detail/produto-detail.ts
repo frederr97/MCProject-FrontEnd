87461ca6ac81af75b3a647450f03e1569bbbfdb8
fredribeiro97@gmail.com <fredribeiro97@gmail.com>
2019-01-11 11:08:04 -0200

    Cart page

--- src/pages/produto-detail/produto-detail.ts
+++ src/pages/produto-detail/produto-detail.ts
@@ -3,6 +3,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { ProdutoDTO } from '../../models/produto.dto';
 import { ProdutoService } from '../../services/domain/produto.service';
 import { API_CONFIG } from '../../config/api.config';
+import { CartService } from '../../services/domain/cart.service';
 
 /**
  * Generated class for the ProdutoDetailPage page.
@@ -20,7 +21,7 @@ export class ProdutoDetailPage {
 
   item: ProdutoDTO;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public produtoService: ProdutoService) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public produtoService: ProdutoService, public cartService: CartService) {
   }
 
   ionViewDidLoad() {
@@ -37,5 +38,10 @@ export class ProdutoDetailPage {
       this.item.imageUrl = `${API_CONFIG.bucketBaseUrl}/prod${this.item.id}.jpg`;
     },
     error => {});
+  }
+
+  addToCart(produto: ProdutoDTO){
+    this.cartService.addProduto(produto);
+    this.navCtrl.setRoot('CartPage');
   }
 }
